8c9a8a3b0c9fe2f7dcaa1b395b5db8175bd3e5bd
FabK02 <fabioblu02@gmail.com>
2024-07-08 10:14:50 +0200

    addedd multiple angles data

--- main.cpp
+++ main.cpp
@@ -4,32 +4,38 @@
 #include<fstream>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include <vector>
+#include <algorithm>
 
 int main(){
-    std::string filename = ("data.txt");
-    std::ofstream file;
-    file.open(filename);
     
     double row{0.1};
     int n_iter{600};
-    double angle{0.4};
+    std::vector<double> angles = {0.2525, 0.4};
+    //double angle{0.4};
 
-    while (row < 0.6){
-        Point start_p{row,0};
-        for (int iter = 0; iter < n_iter; iter++)
-        {
-            file << start_p.x << ";" << start_p.y << '\n';
-            start_p = henon(start_p, 2*M_PI*angle);
-            if (start_p.x > 1 || start_p.y > 1)
+    std::for_each(angles.begin(), angles.end(), [&](double angle){
+        std::string filename = ("data/"+ std::to_string(angle) +"_data.txt");
+        std::ofstream file;
+        file.open(filename);
+
+        while (row < 0.6){
+            Point start_p{row,0};
+            for (int iter = 0; iter < n_iter; iter++)
             {
-                break;
+                file << start_p.x << ";" << start_p.y << '\n';
+                start_p = henon(start_p, 2*M_PI*angle);
+                if (start_p.x > 1 || start_p.y > 1)
+                {
+                    break;
+                }
+                
             }
             
+            row += 0.05;
         }
-        
-        row += 0.05;
-    }
+        file.close();        
+    });
     
-    file.close();        
 }
 
